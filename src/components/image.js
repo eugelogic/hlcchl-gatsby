a84f528a35f42061380d031acd1d71905a29702f
Eugene Molari <eugelogic@gmail.com>
2019-01-09 23:14:10 +0000

    Edit homepage content & remove redundant assets

--- src/components/image.js
+++ src/components/image.js
@@ -17,9 +17,9 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        placeholderImage: file(relativePath: { eq: "Holly-Lodge-Community-Centre-hompepage-banner.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 300) {
+            fluid(maxWidth: 600) {
               ...GatsbyImageSharpFluid
             }
           }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,11 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <h1>The Holly Lodge Community Centre offers a whole range of activities in a friendly,
+relaxed environment. We also offer space to hire for events.</h1>
+    <h2>LATEST ANNOUNCEMENTS:</h2>
+    <p>The HLCCHL will change its status to a Tenants and Residents Hall soon, associated with the HLRA.</p>
+    <div style={{ maxWidth: `600px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
