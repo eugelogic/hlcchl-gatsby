e2ac163a4da87ef532a20d48fe1737b99fb16f8d
Eugene Molari <eugelogic@gmail.com>
2019-01-11 18:25:22 +0000

    Create Footer component

---
+++ src/components/footer.js
@@ -0,0 +1,37 @@
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Footer = () => (
+  <div
+    style={{
+        color: `#8c8989`,
+        background: `#282a2b`,
+        textAlign: `center`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <footer style={{
+          margin: 0,
+          fontSize: `14px`
+       }}>
+        <p style={{ marginBottom: `5px` }}>Holly Lodge Community Centre is a registered charity 293003 2013-2017</p>
+        <p style={{ marginBottom: 0 }}>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a style={{ color: `#8c8989` }} href="https://www.gatsbyjs.org" target="_black">
+                Gatsby
+            </a>
+        </p>
+      </footer>
+    </div>
+  </div>
+)
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
+import Footer from './footer'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -28,12 +29,8 @@ const Layout = ({ children }) => (
           }}
         >
           {children}
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
         </div>
+        <Footer />
       </>
     )}
   />
