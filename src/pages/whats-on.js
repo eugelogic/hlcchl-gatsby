c241e1b84e26d8c69fde3d1fed519820a9db414c
Eugene Molari <eugelogic@gmail.com>
2019-01-27 11:22:55 +0000

    Add Events component

--- src/pages/whats-on.js
+++ src/pages/whats-on.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import LayoutPage from '../components/layout-page/layout-page'
+import Events from '../components/events/events'
 import SEO from '../components/seo'
 
 const WhatsOnPage = () => (
   <LayoutPage pageTitle="Whats On">
     <SEO title="Whats On" />
-    <p>Lorem ipsum cesar salad and dolores mortis.</p>
+    <Events />
   </LayoutPage>
 )
 
