b5afab3eb5da76c81a6cf0cc13b75f58da8ad080
Eugene Molari <eugelogic@gmail.com>
2019-03-24 22:17:33 +0000

    Fix for 'no content a11y warning'

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,19 +2,19 @@ import React from 'react'
 import { Link } from 'gatsby'
 import './button.scss'
 
-const Button = ({ type, color, link, ...other }) => {
+const Button = ({ type, color, link, children, ...other }) => {
 
   if (type === 'submit') {
 
-    return <button type="submit" className={color} {...other} />
+    return <button type="submit" className={color}>{children}</button>
 
   } else if (type === 'a') {
 
-    return <a href={link} className={color} {...other} />;
+    return <a href={link} className={color} {...other}>{children}</a>;
 
   }
 
-  return <Link to={link} className={color} {...other} />;
+  return <Link to={link} className={color} {...other}>{children}</Link>;
 
 }
 
