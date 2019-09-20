aac1bbf545d114f1d41c03edcef00dc65ffd7b55
Eugene Molari <eugelogic@gmail.com>
2019-09-20 23:08:11 +0100

    Update Film Club entries

--- src/pages/film-club.js
+++ src/pages/film-club.js
@@ -12,15 +12,21 @@ const FilmClubPage = () => (
     <small>All films shown on Fridays unless otherwise stated.</small>
     <hr />
     <h2>Upcoming dates</h2>
+    <p><strong>22nd November @ 14:00</strong></p>
+    <p>"An American In Paris" starring Gene Kelly and Leslie Caron.</p>
+    <p><strong>8th November @ 14:00</strong></p>
+    <p>"The Untouchables" starring Kevin Costner and Sean Connery.</p>
+    <p><strong>25th October @ 14:00</strong></p>
+    <p>"Phantom Thread" starring Daniel Day-Lewis.</p>
     <p><strong>11th October @ 14:00</strong></p>
     <p>"La Vie En Rose" starring Marion Cotillard.</p>
     <p><strong>27th September @ 14:00</strong></p>
     <p>"Saps At Sea and 'Music' Shorts" with Laurel & Hardy.</p>
-    <p><strong>13th September @ 14:00</strong></p>
-    <p>"I'm All Right Jack" starring Peter Sellers and Ian Carmichael.</p>
     <hr />
     <h2>Past viewings</h2>
     <p>This is what you've missed on but do not despair some movies will come back.</p>
+    <p><strong>13th September @ 14:00</strong></p>
+    <p>"I'm All Right Jack" starring Peter Sellers and Ian Carmichael.</p>
     <p><strong>9th of August @ 14:00</strong></p>
     <p>"The Quiet Man" starring John Wayne and Maureen O'Hara.</p>
     <p><strong>26th of July @ 14:00</strong></p>
