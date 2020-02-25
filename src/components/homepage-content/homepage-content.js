192375a71e75efcb388b8b24b458cd08d676f15d
Eugene Molari <eugelogic@gmail.com>
2020-02-25 10:58:02 +0000

    Make Download Brochure link open in a new tab

--- src/components/homepage-content/homepage-content.js
+++ src/components/homepage-content/homepage-content.js
@@ -7,7 +7,7 @@ const HomepageContent = () => (
 
     <h3>LATEST ANNOUNCEMENTS:</h3>
 
-    <p>Earthen Living presents a workshop on Living Ayurveda,<br /> an ancient Indian system of healthcare and it's relevance to you today. <br />Join us on Saturday the 7th of March from 11am till noon. <br />For more details get in touch at <a href="mailto:contact@earthenliving.uk">contact@earthenliving.uk</a>. <br />Venue: The Holly Lodge Community Centre - <a href="https://www.google.com/maps/place/Holly+Lodge+Community+Centre/@51.5646877,-0.1504739,17z/data=!3m1!4b1!4m5!3m4!1s0x48761a5175b7d52f:0xdec4c5d5e49a058a!8m2!3d51.5646844!4d-0.1482852" target='_blank' rel='noopener noreferrer'>[see on the map]</a><br /><a href="/living-ayurveda-workshop.pdf">Download the brochure.</a></p>
+    <p>Earthen Living presents a workshop on Living Ayurveda,<br /> an ancient Indian system of healthcare and it's relevance to you today. <br />Join us on Saturday the 7th of March from 11am till noon. <br />For more details get in touch at <a href="mailto:contact@earthenliving.uk">contact@earthenliving.uk</a>. <br />Venue: The Holly Lodge Community Centre - <a href="https://www.google.com/maps/place/Holly+Lodge+Community+Centre/@51.5646877,-0.1504739,17z/data=!3m1!4b1!4m5!3m4!1s0x48761a5175b7d52f:0xdec4c5d5e49a058a!8m2!3d51.5646844!4d-0.1482852" target='_blank' rel='noopener noreferrer'>[see on the map]</a><br /><a href="/living-ayurveda-workshop.pdf" target='_blank'>Download the brochure.</a></p>
 
     <hr />
 
